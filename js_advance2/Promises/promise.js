cd4f81b8b7e8a413bfa7aaf35e87e6f6fc48ff68
Abhipal23 <134617405+Abhipal23@users.noreply.github.com>
2024-05-12 11:09:42 +0530

    Update promise.js

--- js_advance2/Promises/promise.js
+++ js_advance2/Promises/promise.js
@@ -1,24 +1,24 @@
 // Promise is opbject represnting the eventual completation or failure of asyc operation
 // Promise is createad and consumed
 // lets see how to create promise
-// const promiseOne = new Promise(function(resolve,reject){
-//     // do async call
-//     // db calls, cryoptography related
-//     // network releated 
+const promiseOne = new Promise(function(resolve,reject){
+    // do async call
+    // db calls, cryoptography related
+    // network releated 
 
-//     setTimeout(function(){
-//         console.log("async task is completed");
-//         resolve()// when this resolve execute then it will be connected to .then
-//     },1000)
+    setTimeout(function(){
+        console.log("async task is completed");
+        resolve()// when this resolve execute then it will be connected to .then
+    },1000)
 
-// })
+})
 
 
 // lets consume above promise 
 
-// promiseOne.then(function(){
-//     console.log('promise is consumed');
-// })
+promiseOne.then(function(){
+    console.log('promise is consumed');
+})
 // then is an call back function 
 // resolve have connection with then 
 
@@ -26,60 +26,60 @@
 
 // another way to create and consume promises
 
-// new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         console.log('async call 2 is completed ')
-//         resolve()
-//     },1000)
-// }).then(function(){
-//     console.log('async 2 is completed');
-// })
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('async call 2 is completed ')
+        resolve()
+    },1000)
+}).then(function(){
+    console.log('async 2 is completed');
+})
 
 
 
 
 // console.log("---------------------");
 
-// const promiseThree = new Promise(function(resolve,reject){
-//     setTimeout(function(){
-//         resolve({username:"abhishek",email:"abhishek@gmail.com"})
-//     },1000)
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"abhishek",email:"abhishek@gmail.com"})
+    },1000)
 
-// })
+})
 
-// promiseThree.then(function(user){
-//     // user is object which we passed in resolve 
-//     console.log(user);
+promiseThree.then(function(user){
+    // user is object which we passed in resolve 
+    console.log(user);
      
-// })
+})
 
 
 
-// const promiseFour = new Promise(function(resolve,reject){
-//        setTimeout(function(){
-//            let error = true
-//            if(!error){
-//                 resolve({username:"abhi",userId:"231"})
-//            }
-//            else{
-//                reject('somthing went wrong ')
-//            }
-//        },1000)
-// })
+const promiseFour = new Promise(function(resolve,reject){
+       setTimeout(function(){
+           let error = true
+           if(!error){
+                resolve({username:"abhi",userId:"231"})
+           }
+           else{
+               reject('somthing went wrong ')
+           }
+       },1000)
+})
 
 
-// promiseFour.then(user=>{
-//     console.log(user);
-//     return user.username
-//     // where this value will be retiun ?...we can apply chainging of then that will catch this valu e
-// }).then((userName)=>{
-//     // this then we get the value which is return by above then 
-//     console.log(userName);
+promiseFour.then(user=>{
+    //console.log(user);
+    return user.username
+    // where this value will be retiun ?...we can apply chainging of then that will catch this valu e
+}).then((userName)=>{
+    // this then we get the value which is return by above then 
+    console.log(userName);
 
-// }).catch((error)=> console.log(error))
-// .finally(()=>console.log('promise is either resolve or rejected ')
-// // this block of code will always execute irrespective of rject or reslove 
-// )
+}).catch((error)=> console.log(error))
+.finally(()=>console.log('promise is either resolve or rejected ')
+// this block of code will always execute irrespective of rject or reslove 
+)
 
 const promiseFive = new Promise(function(resolve,reject){
     setTimeout(function(){
@@ -115,21 +115,21 @@ async function consumePromiseFive(){
 // consumePromiseFive()
 
 
-// how to use with api :
-// async function getUserData(){
-//                try {
-//                 const response  =  await fetch('https://jsonplaceholder.typicode.com/users')
-//                // console.log(response);
-//                 const data =  await response.json()  // this needs time to convert,so we use await 
-//                 console.log(data);
-//                } catch (error) {
-//                 console.log(error);
+//how to use with api :
+async function getUserData(){
+               try {
+                const response  =  await fetch('https://jsonplaceholder.typicode.com/users')
+               // console.log(response);
+                const data =  await response.json()  // this needs time to convert,so we use await 
+                console.log(data);
+               } catch (error) {
+                console.log(error);
                 
-//                }
+               }
 
-// }
+}
 
-//getUserData();
+getUserData();
 
 // write above thing using then and catch 
 
